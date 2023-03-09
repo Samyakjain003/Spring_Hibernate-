17ebf4575336e4f9fd3679dc3022e965b6ffab11
Samyak Jain <samyakj820@gmail.com>
2023-03-09 16:18:05 +0530

    AOP After Advice

---
+++ spring_demo_aop_after_finally/src/samyak/jain/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package samyak.jain.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("samyak.jain.aopdemo")
+public class DemoConfig {
+
+}
