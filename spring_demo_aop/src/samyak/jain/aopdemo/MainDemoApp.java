2d9ee59762f21eb6ed3a106acb2ed97ec8100170
Samyak Jain <samyakj820@gmail.com>
2023-03-09 10:21:10 +0530

    Spring AOP @Before

---
+++ spring_demo_aop/src/samyak/jain/aopdemo/MainDemoApp.java
@@ -0,0 +1,22 @@
+package samyak.jain.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import samyak.jain.aopdemo.dao.AccountDAO;
+
+public class MainDemoApp {
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context=
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		//get the bean from spring container
+		AccountDAO accountDAO=context.getBean("accountDAO",AccountDAO.class);
+		
+		//call the business method
+		accountDAO.addAccount();
+		
+		// close the context
+		context.close();
+	}
+}
