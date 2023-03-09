7a58e096fa673b221c7487b4bbdfe081240f5aa4
Samyak Jain <samyakj820@gmail.com>
2023-03-09 23:54:05 +0530

    AOP Around Advice

---
+++ spring_demo_aop_around/src/samyak/jain/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,115 @@
+package samyak.jain.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import samyak.jain.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(3)
+public class MyDemoLoggingAspect {
+
+	@Around("execution(* samyak.jain.aopdemo.service.*.getFortune(..))")
+	public Object aroundGetFortune(
+			ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		
+		String method=proceedingJoinPoint.getSignature().toShortString();
+		System.out.println("----->>> Executing @Around on method: "+method);
+		
+		long begin=System.currentTimeMillis();
+		
+		Object result=proceedingJoinPoint.proceed();
+		
+		long end=System.currentTimeMillis();
+		
+		long duration=end-begin;
+		System.out.println("----> Duration: "+duration/1000.0+ " seconds");
+		
+		return result;
+	}
+	
+	@After("execution(* samyak.jain.aopdemo.dao.AccountDAO.findAccounts(..))")
+	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+		
+		String method=theJoinPoint.getSignature().toShortString();
+		System.out.println("----->>> Executing @After on method: "+method);
+		
+	}
+	
+	
+	@AfterThrowing(
+			pointcut="execution(* samyak.jain.aopdemo.dao.AccountDAO.findAccounts(..))",
+			throwing="e")
+	public void afterThrowingFindAccountsAdvice(
+			JoinPoint theJoinPoint, Throwable e) {
+		
+		String method=theJoinPoint.getSignature().toShortString();
+		System.out.println("----->>> Executing @AfterThrowing on method: "+method);
+		
+		System.out.println("----->>> The exception is: "+e);
+
+	}
+
+	@AfterReturning(
+			pointcut="execution(* samyak.jain.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountAdvice(
+			JoinPoint theJoinPoint, List<Account> result) {
+		
+		String method=theJoinPoint.getSignature().toShortString();
+		System.out.println("----->>> Executing @AfterReturning on method: "+method);
+		
+		System.out.println("----->>> Result is: "+result);
+
+		covertAccountNamesToUpperCase(result);
+	}
+	
+	private void covertAccountNamesToUpperCase(List<Account> result) {
+
+		for(Account a:result) {
+			a.setName(a.getName().toUpperCase());
+		}
+		
+	}
+
+	@Before("samyak.jain.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		
+		System.out.println("\n---->>> Executing @Before advice on method");
+		
+		//display the method signature
+		MethodSignature methodSig=(MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: "+methodSig);
+		
+		//display method arguments
+		
+		Object[] args=theJoinPoint.getArgs();
+		for(Object tempArg:args) {
+			System.out.println(tempArg);
+			if(tempArg instanceof Account) {
+				Account theAccount=(Account) tempArg;
+				
+				System.out.println("Account name: "+theAccount.getName());
+				System.out.println("Account level: "+theAccount.getLevel());
+
+			}
+		}
+		
+	}
+	
+	
+	
+}
